75f3fd4094b42734037ad883a501c2910159f704
John Edward Banaag <johnedward.banaag@dkmetrics.com>
2024-02-10 05:25:59 +0800

    Update - Header

--- component/Header.jsx
+++ component/Header.jsx
@@ -32,12 +32,12 @@ export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <header className="bg-white">
+    <header className="" style={{"background-color":"transparent"}}>
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
-            <img className="h-8 w-auto" src={logoImg} alt="" />
+            <img className="h-8 w-auto rounded shadow-lg" src={logoImg} alt="" />
             {/* <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" /> */}
           </a>
         </div>
