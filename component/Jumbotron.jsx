cd30723303197cd09817ac8698b761a22654f975
John Edward Banaag <johnedward.banaag@dkmetrics.com>
2024-02-10 05:26:33 +0800

    Edit - Jumbotron

---
+++ component/Jumbotron.jsx
@@ -0,0 +1,36 @@
+
+
+function Jumbotron() {
+
+    return (
+
+        <section class="" style={{"background-color":"transparent"}}>
+            <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 relative">
+                <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+                    We believe in unlocking global possibilities.
+                </h1>
+                <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">
+                    We are very proud to showcase our productivity tools for your current manufacturing environment. With us, your vision of bringing state-of-the-art technology to optimize your productivity is now a reality.
+                </p>
+                <div class=" space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+                    <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-semibold text-center text-white rounded-lg bg-sky-500 hover:bg-sky-800 focus:ring-4 focus:ring-blue-sky dark:focus:ring-sky-900">
+                       Let's Explore
+
+                        {/* <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                        </svg> */}
+                    </a>
+                </div>
+            </div>
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+            <br />
+        </section>
+    );
+};
+
+
+export default Jumbotron;
